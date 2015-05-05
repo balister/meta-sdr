df7f84072237639627c53e1e3b3bd501b4b9f83f
Philip Balister <philip@balister.org>
2015-05-05 13:14:08 -0400

    build-all.sh: Add error checking to stop broken builds.
    Signed-off-by: Philip Balister <philip@balister.org>

--- contrib/scripts/build-all.sh
+++ contrib/scripts/build-all.sh
@@ -1,16 +1,35 @@
 export MACHINE=zedboard-zynq7
 
-bitbake gnuradio-demo-image
+if ! bitbake gnuradio-demo-image; then
+	echo Failed building image for $MACHINE
+	exit 1
+fi
 
-wic create ../meta-sdr/contrib/wks/sdimage-8G.wks  -e gnuradio-demo-image -o images/$MACHINE
+rm -rf images/$MACHINE/build
+if ! wic create ../meta-sdr/contrib/wks/sdimage-8G.wks  -e gnuradio-demo-image -o images/$MACHINE; then
+	echo Failed building sd card for $MACHINE
+	exit 1
+fi
+
 xz images/$MACHINE/build/sdimage*direct
 
-export MACHINE=zc702-zynq7
+MACHINE=zc702-zynq7
 
-bitbake gnuradio-demo-image
+if ! bitbake gnuradio-demo-image; then
+	echo Failed building image for $MACHINE
+	exit 1
+fi
 
-wic create ../meta-sdr/contrib/wks/sdimage-8G.wks  -e gnuradio-demo-image -o images/$MACHINE
+rm -rf images/$MACHINE/build
+if ! wic create ../meta-sdr/contrib/wks/sdimage-8G.wks  -e gnuradio-demo-image -o images/$MACHINE; then
+	echo Failed building sd card for $MACHINE
+	exit 1
+fi
+
 xz images/$MACHINE/build/sdimage*direct
 
-bitbake -c populate_sdk gnuradio-demo-image
+if ! bitbake -c populate_sdk gnuradio-demo-image; then
+	echo Failed building sdk
+	exit 1
+fi
 
