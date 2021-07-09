d607f2aea4b069604a9583bab8f9db814d01e001
Philip Balister <philip@balister.org>
2021-07-09 14:42:15 -0400

    run-qemu-ptest.sh: Allocate more memory for qemu and logging results.
     * Plan to log results by commiting to a public git repository.
    
    Signed-off-by: Philip Balister <philip@balister.org>

--- contrib/scripts/run-qemu-ptest.sh
+++ contrib/scripts/run-qemu-ptest.sh
@@ -3,10 +3,17 @@
 mkdir -p ptest-output/$MACHINE
 cd ptest-output/$MACHINE
 
-runqemu gnuradio-ptest-image &
+runqemu qemuparams="-m 2048" gnuradio-ptest-image &
 
 sleep 45
 
 ssh root@192.168.7.2 'ptest-runner; shutdown -hf now' | tee ptest.log
 
+sed -i s/[0-9]*\.[0-9][0-9].sec// ptest.log
+
+if [ -d ../.git ]; then
+	git checkout -b $BRANCH
+	git add ptest.log
+	(cd ../..; git log HEAD -1) | git commit -F -
+fi
 
