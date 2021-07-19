b3953cd100c327b158eddcf37b89948e41153709
Philip Balister <philip@balister.org>
2021-07-19 16:45:05 -0400

    run-qemu-ptest.sh: Update testing script.
     * Increase timeout for ptests to try and avoid timeouts
     * Filter messages from fftw ptest to reduce uneccessary diffs between
       runs.
    
    Signed-off-by: Philip Balister <philip@balister.org>

--- contrib/scripts/run-qemu-ptest.sh
+++ contrib/scripts/run-qemu-ptest.sh
@@ -7,9 +7,11 @@ runqemu qemuparams="-m 2048" gnuradio-ptest-image &
 
 sleep 45
 
-ssh root@192.168.7.2 'ptest-runner; shutdown -hf now' | tee ptest.log
+ssh root@192.168.7.2 'ptest-runner -t 1000; shutdown -hf now' | tee ptest.log
 
 sed -i s/[0-9]*\.[0-9][0-9].sec// ptest.log
+sed -i '/^BEGIN: \/usr\/lib\/fftw\/ptest/,/^fftw  test result:/{//!d}' ptest.log
+
 
 if [ -d ../.git ]; then
 	git checkout $BRANCH
